9f187fa20616b2ce866e852c3068574df7c1d499
Erfan Manouchehri <erfan2004@yahoo.com>
2025-08-03 15:10:10 +0330

    changing main to show th main screen not home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'screens/login_screen.dart';
-import 'screens/home_screen.dart';
+import 'package:exchange/screens/main_screen.dart';
 
 void main() {
-  runApp(const CryptoApp());
+  runApp(const MyApp());
 }
 
-class CryptoApp extends StatelessWidget {
-  const CryptoApp({super.key});
-
-  Future<bool> _hasApiKey() async {
-    final storage = FlutterSecureStorage();
-    final key = await storage.read(key: 'api_key');
-    return key != null && key.isNotEmpty;
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nobitex Crypto App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: FutureBuilder<bool>(
-        future: _hasApiKey(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return const CircularProgressIndicator();
-          return snapshot.data! ? const HomeScreen() : const LoginScreen();
-        },
+      title: 'Nobitex Exchange',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Vazirmatn',
       ),
+      home: const MainScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
