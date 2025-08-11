ff9f1f18d5b360a4683c1d9822cffd0c395595e5
Erfan Manouchehri <erfan2004@yahoo.com>
2025-08-11 18:32:37 +0330

    chart model

---
+++ lib/models/ohlc_data.dart
@@ -0,0 +1,15 @@
+class OhlcData {
+  final DateTime time;
+  final double open;
+  final double high;
+  final double low;
+  final double close;
+
+  OhlcData({
+    required this.time,
+    required this.open,
+    required this.high,
+    required this.low,
+    required this.close,
+  });
+}
