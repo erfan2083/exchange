032b61a5766d57bfe40f989ef4e1a74bbd5b8a2d
Erfan Manouchehri <erfan2004@yahoo.com>
2025-08-11 17:43:17 +0330

    working on it

--- lib/screens/coin_detail_screen.dart
+++ lib/screens/coin_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/coin_stats.dart';
@@ -23,45 +25,60 @@ class _CoinDetailScreenState extends State<CoinDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('${widget.symbol.toUpperCase()} / IRT')),
-      body: FutureBuilder<CoinStats>(
-        future: futureStats,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return Center(child: CircularProgressIndicator());
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF121330), // top color
+            Color(0xFF3E1E68), // bottom color
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(title: Text('${widget.symbol.toUpperCase()} / IRT'),
+        backgroundColor: Colors.transparent
+          ,),
+        backgroundColor: Colors.transparent,
+        body: FutureBuilder<CoinStats>(
+          future: futureStats,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return Center(child: CircularProgressIndicator());
 
-          if (snapshot.hasError)
-            return Center(child: Text('Error: ${snapshot.error}'));
+            if (snapshot.hasError)
+              return Center(child: Text('Error: ${snapshot.error}'));
 
-          final coin = snapshot.data!;
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Current Price: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.latest)} Rials',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 10),
-                Text('Buy: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.bestBuy)} | Sell: ${coin.bestSell}'),
-                const SizedBox(height: 10),
-                Text('High: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.dayHigh)} | Low: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.dayLow)}'),
-                const SizedBox(height: 10),
-                Text('Change: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.dayChange)}%', style: TextStyle(
-                    color: coin.dayChange >= 0 ? Colors.green : Colors.red
-                )),
-                const Spacer(),
-                Row(
-                  children: [
-                    Expanded(child: ElevatedButton(onPressed: () {}, child: Text('Buy'))),
-                    const SizedBox(width: 10),
-                    Expanded(child: OutlinedButton(onPressed: () {}, child: Text('Sell'))),
-                  ],
-                )
-              ],
-            ),
-          );
-        },
+            final coin = snapshot.data!;
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Current Price: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.latest)} Rials',
+                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 10),
+                  Text('Buy: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.bestBuy)} | Sell: ${coin.bestSell}'),
+                  const SizedBox(height: 10),
+                  Text('High: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.dayHigh)} | Low: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.dayLow)}'),
+                  const SizedBox(height: 10),
+                  Text('Change: ${NumberFormat.currency(locale: 'en_US', name: '', decimalDigits: 0).format(coin.dayChange)}%', style: TextStyle(
+                      color: coin.dayChange >= 0 ? Colors.green : Colors.red
+                  )),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      Expanded(child: ElevatedButton(onPressed: () {}, child: Text('Buy'))),
+                      const SizedBox(width: 10),
+                      Expanded(child: OutlinedButton(onPressed: () {}, child: Text('Sell'))),
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
